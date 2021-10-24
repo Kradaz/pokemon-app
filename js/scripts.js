e5b3aa7727ad980ba2ae13196975f8ca01f9f1d0
Kradaz <adarshsaulick@hotmail.it>
2021-10-24 18:26:36 +0100

    Added pokemon array
    4 pokemons "mewtwo, zapdos, palkia and gengar" added with their respective name, height, type and ability

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+  {name: "Mewtwo", height: 7, type: "psychic", ability: "Pressure"},
+  {name: "Zapdos", height: 1.6, type: ["psychic", "flying"], ability: "Lightningrod"},
+  {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
+  {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
+]
