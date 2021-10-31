99648eb0ca3cb6e0aa9e9b5d8f6e3bc6180a5e85
Kradaz <adarshsaulick@hotmail.it>
2021-10-31 18:31:34 +0000

    Check Pokemon height
    Put a note on a Pokemon who's height is  equal to 7

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,15 @@ let pokemonList = [
 // FOR LOOP TO DISPLAY POKEMONLIST
 let text = " ";
 for (let i = 0; i < pokemonList.length; i++){
+ if(pokemonList[i].height <7 && pokemonList[i].height >1){
   text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>");
+} else if (pokemonList[i].height == 7){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " This pokemon is Big!!" + "<br>");
+} else {
   document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>");
+}
 }
 
 // END OF FOR LOOP
