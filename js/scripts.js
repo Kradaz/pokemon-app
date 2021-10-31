936589915b1789432061921c7eb3ce99afbefce5
Kradaz <adarshsaulick@hotmail.it>
2021-10-31 18:02:06 +0000

    For loop
    Displaying pokemonlist through for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ let pokemonList = [
   {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
   {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
 ]
+
+// FOR LOOP TO DISPLAY POKEMONLIST
+let text = " ";
+for (let i = 0; i < pokemonList.length; i++){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " Type: " + pokemonList[i].type + " Ability: " + pokemonList[i].ability + "<br>");
+}
+
+// END OF FOR LOOP
