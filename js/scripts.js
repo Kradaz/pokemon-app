7f937f170ac51047c611274b87434624194cc33b
Kradaz <adarshsaulick@hotmail.it>
2021-11-01 18:04:37 +0000

    Line 14 == to ===

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ for (let i = 0; i < pokemonList.length; i++){
  if(pokemonList[i].height <7 && pokemonList[i].height >1){
   text = pokemonList[i];
   document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>" + "<br>");
-} else if (pokemonList[i].height == 7){
+} else if (pokemonList[i].height === 7){
   text = pokemonList[i];
   document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " This pokemon is Big!!" + "<br>" + "<br>");
 } else {
