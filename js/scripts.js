506cbb32f9912a803d122f68b683510a215b497c
Kradaz <adarshsaulick@hotmail.it>
2021-11-01 17:55:28 +0000

    Added space into pokemon names

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,12 @@ let text = " ";
 for (let i = 0; i < pokemonList.length; i++){
  if(pokemonList[i].height <7 && pokemonList[i].height >1){
   text = pokemonList[i];
-  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>");
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>" + "<br>");
 } else if (pokemonList[i].height == 7){
   text = pokemonList[i];
-  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " This pokemon is Big!!" + "<br>");
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " This pokemon is Big!!" + "<br>" + "<br>");
 } else {
-  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>");
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>" + "<br>");
 }
 }
 
