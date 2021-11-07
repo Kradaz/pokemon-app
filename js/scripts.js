b4285b0c206eb0009a72965bc3d343576a504bd9
Kradaz <adarshsaulick@hotmail.it>
2021-11-07 14:22:32 +0000

    Implemented Repository
    Stored pokemon list safely into a repository. Updated forEach loop to access pokemon list inside repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
+let pokemonRepository = (function() {
 let pokemonList = [
   {name: "Mewtwo", height: 7, type: "psychic", ability: "Pressure"},
   {name: "Zapdos", height: 1.6, type: ["psychic", "flying"], ability: "Lightningrod"},
   {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
   {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
-]
+];
+//Return every item from the list
+function getAll() {
+  return pokemonList;
+}
+
+function add(pokemon){
+  pokemonList.push(pokemon);
+}
+//Return values outside
+return {
+  getAll: getAll,
+  add: add
+};
+})();
 
 // LOOP OVER ARRAY DISPLAYING ALL POKEMON AND THEIR DETAILS
 function myLoopFunction(pokemon) {
   document.write(' Name: '+ pokemon.name + ' - Height: ' + pokemon.height + ' - Type: ' + pokemon.type + ' - Ability: ' + pokemon.ability + '<br>');
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction); // CONNECT POKEMONLIST TO OUTSIDE OF THE REPOSITORY THROUGH getAll AND forEach
 // END OF LOOP
