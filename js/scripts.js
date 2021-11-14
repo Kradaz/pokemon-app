74be47b8f0eca840f44584180218158ddefcbc45
Kradaz <adarshsaulick@hotmail.it>
2021-11-14 17:30:36 +0000

    Implementation of modal
    Modal assigned to pokemon to display name height and image of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,57 @@ function addEventItem(button, pokemon){
 
 function showDetails(pokemon){
   loadDetails(pokemon).then(function(){
-  console.log(pokemon);
+  showModal(pokemon);
 });
+
+// MODAL CODE PIECE
+let modalContainer = document.querySelector('#modal-container');
+function showModal(pokemon) {
+  modalContainer.innerHTML = '';
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = pokemon.name;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = "Height: " + pokemon.height;
+
+  let pokemonImage = document.createElement('img');
+  pokemonImage.src = pokemon.imageUrl;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modal.appendChild(pokemonImage);
+  modalContainer.appendChild(modal);
+
+
+  modalContainer.classList.add('is-visible');
+}
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+modalContainer.addEventListener('click', (e) => {
+
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
 }
 // Promise added to fetch the data from the API using json
 function loadList() {
@@ -62,6 +111,7 @@ function loadDetails(item) {
     console.error(e);
   });
 }
+
 //Return values outside
 return {
   getAll: getAll,
