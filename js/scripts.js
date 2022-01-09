2ad1c5dea63103ffdc4bc6f9f61daa9138142233
Kradaz <adarshsaulick@hotmail.it>
2022-01-09 16:27:46 +0000

    Updated modal
    Changed into modal through boostrap. Updated modal to show pokemon pictures on the web buttons. Deleted old piece of codes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonRepository = (function() {
+//let modalContainer = document.querySelector('#exampleModal');
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 //Return every item from the list
@@ -14,12 +15,32 @@ function addListItem(pokemon){
   let pokemonList = document.querySelector(".pokemon-list");
   let listpokemon = document.createElement("li");
   let button = document.createElement("button");
+  let pokemonImg = document.createElement("img");
+
+
   button.innerText = pokemon.name;
   button.classList.add("button-class");
+  button.classList.add("btn");
+  pokemonImg.classList.add("btn-img");
+
+
+  button.appendChild(pokemonImg);
   listpokemon.appendChild(button);
   pokemonList.appendChild(listpokemon);
   addEventItem(button, pokemon);
+
+// displaying picture of pokemon into the webpage
+  let url = pokemon.detailsUrl;
+return fetch(url).then(function(response){
+  return response.json();
+}).then(function(details){
+  pokemon.image = details.sprites.front_default;
+  pokemonImg.setAttribute ('src', pokemon.image);
+}).catch(function(e){
+  console.error(e);
+});
 }
+
 
 function addEventItem(button, pokemon){
   button.addEventListener("click", function(){
@@ -32,54 +53,32 @@ function showDetails(pokemon){
   showModal(pokemon);
 });
 
-// MODAL CODE PIECE
-let modalContainer = document.querySelector('#modal-container');
-function showModal(pokemon) {
-  modalContainer.innerHTML = '';
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-  closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
-
-  let titleElement = document.createElement('h1');
-  titleElement.innerText = pokemon.name;
-
-  let contentElement = document.createElement('p');
-  contentElement.innerText = "Height: " + pokemon.height;
-
-  let pokemonImage = document.createElement('img');
-  pokemonImage.src = pokemon.imageUrl;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(titleElement);
-  modal.appendChild(contentElement);
-  modal.appendChild(pokemonImage);
-  modalContainer.appendChild(modal);
 
 
-  modalContainer.classList.add('is-visible');
+let modalContainer = document.querySelector('#exampleModal');
+function showModal(pokemon){
+  let modalBody = $('.modal-body');
+  let modalTitle = $('.modal-title');
+  let modalHeader = $('.modal-header');
+
+  modalBody.empty();
+  modalTitle.empty();
+
+  let nameElement = $('<h1 class="text-capitalize">' + pokemon.name + '</h1>');
+  let imageElement = $('<img class="modal-img" style="width:30%">');
+  imageElement.attr("src", pokemon.imageUrl);
+
+  let heightElement = $('<p>' + 'Height : ' + pokemon.height + '</p>');
+  let weightElement = $('<p>' + 'Weight : ' + pokemon.weight + '</p>');
+
+  modalBody.append(imageElement);
+  modalTitle.append(nameElement);
+  modalBody.append(heightElement);
+  modalBody.append(weightElement);
+
+  $('#exampleModal').modal();
+
 }
-
-function hideModal() {
-  modalContainer.classList.remove('is-visible');
-}
-
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-    hideModal();
-  }
-});
-modalContainer.addEventListener('click', (e) => {
-
-  let target = e.target;
-  if (target === modalContainer) {
-    hideModal();
-  }
-});
-
 }
 // Promise added to fetch the data from the API using json
 function loadList() {
@@ -89,7 +88,7 @@ function loadList() {
     json.results.forEach(function (item) {
       let pokemon = {
         name: item.name,
-        detailsUrl: item.url
+        detailsUrl: item.url,
       };
       add(pokemon);
     });
@@ -107,6 +106,7 @@ function loadDetails(item) {
     item.imageUrl = details.sprites.front_default;
     item.height = details.height;
     item.types = details.types;
+    item.weight = details.weight;
   }).catch(function (e) {
     console.error(e);
   });
